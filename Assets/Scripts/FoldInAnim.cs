36951dc34f9dfc00c0cbba9aff41fe8d4bbc5a04
HYrecovery <mcfengye1003@outlook.com>
2026-04-27 22:14:17 +0800

    Completed Timer-based animation systems, added segments, with additional control to the anims.

--- Assets/Scripts/FoldInAnim.cs
+++ Assets/Scripts/FoldInAnim.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class FoldInAnim : MonoBehaviour
 {
@@ -7,10 +8,20 @@ public class FoldInAnim : MonoBehaviour
     public Image img;
     public Camera targetCamera;
     Quaternion camRot;
-    public float offset = 45;
+    public float initOffset = 45;
+    float offset;
+    public float initSizeRatio = 1.3f;
+    public float initImageRatio = 1.5f;
+    public float TransparencyAnimDuration = 2.0f;
+    public float ResizeDuration = 2.0f;
+    public float FoldAnimDuration = 2.0f;
+    public float AfterAnimDuration = 10.0f;
+    public bool UseSmoothFormula = false;
     public bool startAnimation = false;
     bool isOnAnimStart = true;
     public FoldDirection foldDirection = FoldDirection.Clockreverse;
+    public bool isInitialized = true;
+    float timer = 0;
 
     public enum FoldDirection
     {
@@ -27,21 +38,35 @@ public class FoldInAnim : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        InitializeAnim();
+    }
+
+    void InitializeAnim()
+    {
+        offset = initOffset;
+        startAnimation = false;
+        isOnAnimStart = true;
+        timer = 0;
         camRot = targetCamera.transform.rotation;
         canvasGroup = gameObject.GetComponent<CanvasGroup>();
         canvasGroup.alpha = 0;
         //gameObject.transform.localRotation = new(0, 45, 0, 0);
         //gameObject.transform.forward = new(0, -1, 0);
-        Debug.Log(gameObject.transform.rotation);
-        Debug.Log(gameObject.transform.forward);
-        gameObject.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
-        img.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        //Debug.Log(gameObject.transform.rotation);
+        //Debug.Log(gameObject.transform.forward);
+        gameObject.transform.localScale = new Vector3(initSizeRatio, initSizeRatio, initSizeRatio);
+        img.transform.localScale = new Vector3(initImageRatio, initImageRatio, initImageRatio);
         //transform.rotation = camRot * Quaternion.Euler(0, offset, 0);
+        isInitialized = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isInitialized)
+        {
+            InitializeAnim();
+        }
         if (!startAnimation)
         {
             return;
@@ -54,31 +79,67 @@ public class FoldInAnim : MonoBehaviour
                 isOnAnimStart = false;
             }
         }
-        if (canvasGroup.alpha < 1.0f)
+        timer += UnityEngine.Time.deltaTime;
+        if (timer <= TransparencyAnimDuration)
         {
-            canvasGroup.alpha += 0.005f;
-        }
-        if (offset > 0)
-        {
-            offset -= 1f;
-        }
-        DoOffsetSync();
-        if (gameObject.transform.localScale.x >= 1.0f)
-        {
-            gameObject.transform.localScale = new(
-                gameObject.transform.localScale.x - 0.01f,
-                gameObject.transform.localScale.x - 0.01f,
-                gameObject.transform.localScale.x - 0.01f);
+            
+            if (UseSmoothFormula)
+            {
+
+            }
+            else
+            {
+                float percentage = Mathf.Clamp01(timer / TransparencyAnimDuration);
+                canvasGroup.alpha = Mathf.Lerp(0f, 1f, percentage);
+                //Debug.Log($"alpha = {percentage}");
+            }
         }
         else
         {
+            canvasGroup.alpha = 1f;
+        }
 
-            if (img.transform.localScale.x >= 1.0f)
+        if (timer <= FoldAnimDuration) 
+        {
+            if (UseSmoothFormula)
             {
-                img.transform.localScale = new(
-                    img.transform.localScale.x - 0.01f,
-                    img.transform.localScale.x - 0.01f,
-                    img.transform.localScale.x - 0.01f);
+
+            }
+            else
+            {
+                float percentage = Mathf.Clamp01(timer / FoldAnimDuration);
+                offset = Mathf.Lerp(initOffset, 0f, percentage);
+                //Debug.Log($"offset = {percentage}");
+            }
+        }
+        else
+        {
+            offset = 0f;
+        }
+
+        DoOffsetSync();
+
+        if (timer <= ResizeDuration)
+        {
+            float percentage = Mathf.Clamp01(timer / ResizeDuration);
+            var value = Mathf.Lerp(initSizeRatio, 1f, percentage);
+            gameObject.transform.localScale = new(value, value, value);
+            //Debug.Log($"resize = {percentage}");
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+            if (timer <= ResizeDuration + AfterAnimDuration)
+            {
+                float percentage = Mathf.Clamp01((timer - ResizeDuration) / AfterAnimDuration);
+                var value = Mathf.Lerp(initImageRatio, 1f, percentage);
+                img.transform.localScale = new(value, value, value);
+                //Debug.Log($"after = {percentage}");
+                //Debug.Log($"time = {timer -  ResizeDuration}");
+            }
+            else
+            {
+                img.transform.localScale = new(1f, 1f, 1f);
             }
         }
     }
---
+++ Assets/Scripts/HySliderDaemon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HySliderDaemon : MonoBehaviour
+{
+    int Width = Screen.width;
+    int Height= Screen.height;
+    public GameObject ImgPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
