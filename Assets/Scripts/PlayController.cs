f2e0f7d0cd8de41c261a899850542cad257dce0f
HYrecovery <mcfengye1003@outlook.com>
2026-05-01 20:58:14 +0800

    Enabled Both input packages and exit method.

---
+++ Assets/Scripts/PlayController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
